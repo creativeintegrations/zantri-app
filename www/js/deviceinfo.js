220d2edda5b55f0320b65b09d629768f76125232
subodh-1 <choure.subodh@gmail.com>
2016-10-28 17:23:22 +0530

    all basic set up

---
+++ www/js/deviceinfo.js
@@ -0,0 +1,8 @@
+/*
+ * Author :: Subodh D. Choure
+ * Company :: Creative Integratioins Pvt. Ltd
+ * Created Date :: 28-10-2016
+ * File is created to get all device information to identify the user.
+ * This code is not distributed under not cirumstance and can not be used for any other purpose without author's consent
+ * Copyright : Subodh D. Choure
+ */
